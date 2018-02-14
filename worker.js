eac52cd86e553d81a5f7bd35c67a4339b1355717
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-14 21:16:01 +1100

    Update worker.js with message ack, persistence, fair dispatch

--- worker.js
+++ worker.js
@@ -6,20 +6,32 @@ amqp.connect('amqp://localhost', (err, conn) => {
   // Create channel
   conn.createChannel((err, ch) => {
     // Name of the queue
-    const q = 'task_queue'
+    const q = 'task_queue_durable'
     // Declare the queue
-    ch.assertQueue(q, { durable: false })
+    ch.assertQueue(q, { durable: true }) // { durable: true } ensures that the message will still be redelivered even if RabbitMQ service is turned off/restarted
+    // Tell RabbitMQ not to give more than 1 message per worker
+    ch.prefetch(1)
 
     // Wait for Queue Messages
     console.log(` [*] Waiting for messages in ${q}. To exit press CTRL+C`)
     ch.consume( q, msg => {
+        // Just to simulate a fake task, length of dots in the message
+        // is the number of secs the task will run
         const secs = msg.content.toString().split('.').length - 1
+
         console.log(` [x] Received ${msg.content.toString()}`)
+        console.log(` [x] Task will run for ${secs} secs`)
+        
         // Fake task which simulates execution time
-        setTimeout(function() {
-          console.log(" [x] Done");
+        setTimeout(() => {
+          console.log(` [x] Done ${msg.content.toString()}`);
+          // Send acknowledgment
+          ch.ack(msg)
         }, secs * 1000)
-      }, { noAck: true }
+
+      }, { noAck: false } // noAck: false means Message acknowledgments is turned on
+      // When message acknowledgements are turned on, even if a worker.js is killed (Ctrl+C)
+      // while processing a message, it will be redelivered
     )
   })
 })
