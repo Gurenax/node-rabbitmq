3dde76acb1a7b79235d85bc025f212f16e83b648
Glenn Dimaliwat <glenn.dimaliwat@gmail.com>
2018-02-14 21:15:54 +1100

    Update new_task.js with message ack, persistence, fair dispatch

--- new_task.js
+++ new_task.js
@@ -6,15 +6,15 @@ amqp.connect('amqp://localhost', (err, conn) => {
   // Create channel
   conn.createChannel((err, ch) => {
     // Name of the queue
-    const q = 'task_queue'
+    const q = 'task_queue_durable'
     // Write a message
     const msg = process.argv.slice(2).join(' ') || "Hello World!"
 
     // Declare the queue
-    ch.assertQueue(q, { durable: false })
+    ch.assertQueue(q, { durable: true }) // { durable: true } ensures that the message will still be redelivered even if RabbitMQ service is turned off/restarted
 
     // Send message to the queue
-    ch.sendToQueue(q, new Buffer(msg), {persistent: true})
+    ch.sendToQueue(q, new Buffer(msg), {persistent: true}) // {persistent: true} saves the message to disk/cache
     console.log(` {x} Sent '${msg}'`)
 
     // Close the connection and exit
